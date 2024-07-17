b76935623b00bd18c23538b82d36d0938dfb4490
YusuffAhmad <yusuffahmad2005@gmail.com>
2024-07-17 16:07:43 +0100

    added ef configuration for puchase order

--- src/Domain/Entities/Aggregates/PurchaseOrderAggregate/PurchaseOrderItem.cs
+++ src/Domain/Entities/Aggregates/PurchaseOrderAggregate/PurchaseOrderItem.cs
@@ -3,18 +3,20 @@
     public class PurchaseOrderItem
     {
         public Guid PurchaseOrderItemId { get; private set; }
+        public Guid PurchaseOrderId { get; private set; }
         public string Description { get; private set; }
         public int Quantity { get; private set; }
         public decimal UnitPrice { get; private set; }
         public decimal TotalPrice => Quantity * UnitPrice;
 
         private PurchaseOrderItem() { }
-        public PurchaseOrderItem(string description, int quantity, decimal unitPrice)
+        public PurchaseOrderItem(string description, int quantity, decimal unitPrice, Guid purchaseOrderId)
         {
             PurchaseOrderItemId = Guid.NewGuid();
             Description = description;
             Quantity = quantity;
             UnitPrice = unitPrice;
+            PurchaseOrderId = purchaseOrderId;
         }
     }
 }
