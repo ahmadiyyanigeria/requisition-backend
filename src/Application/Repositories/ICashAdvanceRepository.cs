425b10d0999ab528f61ce31d913f3a856cda28ae
Abdulrohee-Abdullateef <roheemlateef.3@gmail.com>
2024-07-29 01:03:31 +0100

    Endpoint to retrieve a cash advance

--- src/Application/Repositories/ICashAdvanceRepository.cs
+++ src/Application/Repositories/ICashAdvanceRepository.cs
@@ -1,10 +1,12 @@
 ﻿using Domain.Entities.Aggregates.CashAdvanceAggregate;
+using System.Linq.Expressions;
 
 namespace Application.Repositories
 {
     public interface ICashAdvanceRepository
     {
         Task<CashAdvance> AddAsync(CashAdvance cashAdvance);
-        Task<CashAdvance?> GetByIdAsync(Guid cashAdvanceId);       
+        Task<CashAdvance?> GetByIdAsync(Guid cashAdvanceId);
+        Task<CashAdvance?> GetByIdAsync(Expression<Func<CashAdvance, bool>> expression);
     }
 }
